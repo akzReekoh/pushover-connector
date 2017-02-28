7c18208c946e75a9a1bfce7cf43a7adadffbbcef
Achilles Dinglasan <adinglasan@reekoh.com>
2017-02-28 17:42:35 +0800

    migrated to v3

--- app.js
+++ app.js
@@ -1,109 +1,120 @@
-'use strict';
+'use strict'
 
-var platform = require('./platform'),
-    isEmpty = require('lodash.isempty'),
-    isPlainObject = require('lodash.isplainobject'),
-    isNumber = require('lodash.isnumber'),
-    isArray = require('lodash.isarray'),
-    async = require('async'),
-	pushOverClient, defaultMessage;
+let reekoh = require('reekoh')
+let _plugin = new reekoh.plugins.Connector()
+let async = require('async')
+let isArray = require('lodash.isarray')
+let isEmpty = require('lodash.isempty')
+let isNumber = require('lodash.isnumber')
+let isPlainObject = require('lodash.isplainobject')
+let pushOverClient = null
 
 let sendData = (data, callback) => {
-    var msg = {
-        message: '',
-        title: '',
-        sound: '',
-        device: '',
-        priority: '',
-        url: '',
-        url_title: ''
-    };
+  let msg = {
+    message: '',
+    title: '',
+    sound: '',
+    device: '',
+    priority: '',
+    url: '',
+    urlTitle: ''
+  }
 
-    if(isEmpty(data.message))
-        msg.message = defaultMessage;
-    else
-        msg.message = data.message;
+  if (isEmpty(data.message)) {
+    msg.message = _plugin.config.defaultMessage
+  } else {
+    msg.message = data.message
+  }
 
-    if(isEmpty(data.title))
-        delete msg.title;
-    else
-        msg.title = data.title;
+  if (isEmpty(data.title)) {
+    delete msg.title
+  } else {
+    msg.title = data.title
+  }
 
-    if(isEmpty(data.sound))
-        delete msg.sound;
-    else
-        msg.sound = data.sound;
+  if (isEmpty(data.sound)) {
+    delete msg.sound
+  } else {
+    msg.sound = data.sound
+  }
 
-    if(isEmpty(data.device))
-        delete msg.device;
-    else
-        msg.device = data.device;
+  if (isEmpty(data.device)) {
+    delete msg.device
+  } else {
+    msg.device = data.device
+  }
 
-    if(isNumber(data.priority) && data.priority < 3)
-        msg.priority = data.priority;
-    else
-        delete msg.priority;
+  if (isNumber(data.priority) && data.priority < 3) {
+    msg.priority = data.priority
+  } else {
+    delete msg.priority
+  }
 
-    if(isEmpty(data.url))
-        delete  msg.url;
-    else
-        msg.url = data.url;
+  if (isEmpty(data.url)) {
+    delete msg.url
+  } else {
+    msg.url = data.url
+  }
 
-    if(isEmpty(data.url_title))
-        delete  msg.url_title;
-    else
-        msg.url_title = data.url_title;
+  if (isEmpty(data.urlTitle)) {
+    delete msg.urlTitle
+  } else {
+    msg.urlTitle = data.urlTitle
+  }
 
-    pushOverClient.send( msg, function( error, result ) {
-        if (!error){
-            platform.log(JSON.stringify({
-                title: 'Pushover Notification Sent',
-                data: msg
-            }));
-        }
-
-        callback(error);
-    });
-};
-
-platform.on('data', function (data) {
-    if(isPlainObject(data)){
-        sendData(data, (error) => {
-            if(error) {
-                console.error(error);
-                platform.handleException(error);
-            }
-        });
+  pushOverClient.send(msg, (error) => {
+    if (!error) {
+      _plugin.log(JSON.stringify({
+        title: 'Pushover Notification Sent',
+        data: msg
+      }))
     }
-    else if(isArray(data)){
-        async.each(data, (datum, done) => {
-            sendData(datum, done);
-        }, (error) => {
-            if(error) {
-                console.error(error);
-                platform.handleException(error);
-            }
-        });
+
+    callback(error)
+  })
+}
+
+/**
+ * Emitted when device data is received.
+ * This is the event to listen to in order to get real-time data feed from the connected devices.
+ * @param {object} data The data coming from the device represented as JSON Object.
+ */
+_plugin.on('data', (data) => {
+  if (isPlainObject(data)) {
+    sendData(data, (error) => {
+      if (error) {
+        console.error(error)
+        _plugin.logException(error)
+      }
+    })
+  } else if (isArray(data)) {
+    async.each(data, (datum, done) => {
+      sendData(datum, done)
+    }, (error) => {
+      if (error) {
+        console.error(error)
+        _plugin.logException(error)
+      }
+    })
+  } else { _plugin.logException(new Error('Invalid data received. Must be a valid JSON Object. Data ' + data)) }
+})
+
+/**
+ * Emitted when the platform bootstraps the plugin. The plugin should listen once and execute its init process.
+ */
+_plugin.once('ready', () => {
+  let PushOver = require('pushover-notifications')
+
+  pushOverClient = new PushOver({
+    user: _plugin.config.user,
+    token: _plugin.config.token,
+    onerror: (error) => {
+      _plugin.logException(error)
     }
-    else
-        platform.handleException(new Error('Invalid data received. Must be a valid JSON Object. Data ' + data));
-});
+  })
 
-platform.once('close', function () {
-    platform.notifyClose();
-});
+  _plugin.log('Pushover Connector has been initialized.')
+  _plugin.emit('init')
+})
 
-platform.once('ready', function (options) {
-    var PushOver = require('pushover-notifications');
-    defaultMessage = options.default_message;
-    pushOverClient = new PushOver({
-        user : options.user,
-        token : options.token,
-        onerror : function(error){
-            platform.handleException(error);
-        }
-    });
-
-    platform.log('Pushover Connector Initialized.');
-	platform.notifyReady();
-});
+module.exports = _plugin
--- test/pushover-connector.test.js
+++ test/pushover-connector.test.js
@@ -1,95 +1,162 @@
-'use strict';
+'use strict'
 
-const USER = 'udrgrpBG5H2dUdGRAHWgcJhW2HMApz',
-    TOKEN = 'afc7t8RWCxj1ZxKJVUYZqpQ9LsJq6K';
+const amqp = require('amqplib')
 
-var cp     = require('child_process'),
-	assert = require('assert'),
-	connector;
+const USER = 'udrgrpBG5H2dUdGRAHWgcJhW2HMApz'
+const TOKEN = 'afc7t8RWCxj1ZxKJVUYZqpQ9LsJq6K'
 
-describe('Connector', function () {
-	this.slow(5000);
+let _channel = null
+let _conn = null
+let app = null
 
-	after('terminate child process', function (done) {
-		this.timeout(7000);
+describe('HCP MMS Connector Test', () => {
+  before('init', () => {
+    process.env.ACCOUNT = 'adinglasan'
+    process.env.CONFIG = JSON.stringify({
+      user: USER,
+      token: TOKEN
+    })
+    process.env.INPUT_PIPE = 'ip.pushover'
+    process.env.LOGGERS = 'logger1, logger2'
+    process.env.EXCEPTION_LOGGERS = 'ex.logger1, ex.logger2'
+    process.env.BROKER = 'amqp://guest:guest@127.0.0.1/'
 
-        setTimeout(function(){
-            connector.kill('SIGKILL');
-			done();
-        }, 5000);
-	});
+    amqp.connect(process.env.BROKER)
+      .then((conn) => {
+        _conn = conn
+        return conn.createChannel()
+      }).then((channel) => {
+      _channel = channel
+    }).catch((err) => {
+      console.log(err)
+    })
+  })
 
-	describe('#spawn', function () {
-		it('should spawn a child process', function () {
-			assert.ok(connector = cp.fork(process.cwd()), 'Child process not spawned.');
-		});
-	});
+  after('close connection', function (done) {
+    _conn.close()
+    done()
+  })
 
-	describe('#handShake', function () {
-		it('should notify the parent process when ready within 5 seconds', function (done) {
-			this.timeout(5000);
+  describe('#start', function () {
+    it('should start the app', function (done) {
+      this.timeout(10000)
+      app = require('../app')
+      app.once('init', done)
+    })
+  })
 
-			connector.on('message', function (message) {
-				if (message.type === 'ready')
-					done();
-			});
+  describe('#data', () => {
+    it('should send data to third party client', function (done) {
+      this.timeout(15000)
 
-			connector.send({
-				type: 'ready',
-				data: {
-					options: {
-						user: USER,
-						token: TOKEN
-					}
-				}
-			}, function (error) {
-				assert.ifError(error);
-			});
-		});
-	});
+      let data = {
+        message: 'This is a test message from Pushover Connector Plugin',
+        title: 'Test Message',
+        sound: 'default',
+        device: 'm1note',
+        priority: 1,
+        url: 'http://reekoh.com',
+        urlTitle: 'Reekoh Official Website'
+      }
 
-	describe('#data', function (done) {
-		it('should process the JSON data', function () {
-			connector.send({
-				type: 'data',
-				data: {
-                    message: 'This is a test message from Pushover Connector Plugin',
-                    title: 'Test Message',
-                    sound: 'default',
-                    device: 'm1note',
-                    priority: 1,
-                    url: 'http://reekoh.com',
-                    url_title: 'Reekoh Official Website'
-				}
-			}, done);
-		});
-	});
+      _channel.sendToQueue('ip.pushover', new Buffer(JSON.stringify(data)))
+      setTimeout(done, 10000)
+    })
+  })
+})
 
-	describe('#data', function (done) {
-		it('should process the Array data', function () {
-			connector.send({
-				type: 'data',
-				data: [
-					{
-						message: 'This is a test message from Pushover Connector Plugin',
-						title: 'Test Message',
-						sound: 'default',
-						device: 'm1note',
-						priority: 1,
-						url: 'http://reekoh.com',
-						url_title: 'Reekoh Official Website'
-					},
-					{
-						message: 'This is a test message from Pushover Connector Plugin',
-						title: 'Test Message',
-						sound: 'default',
-						device: 'm1note',
-						priority: 1,
-						url: 'http://reekoh.com',
-						url_title: 'Reekoh Official Website'
-					}
-				]
-			}, done);
-		});
-	});
-});
+// 'use strict';
+//
+// const USER = 'udrgrpBG5H2dUdGRAHWgcJhW2HMApz',
+//     TOKEN = 'afc7t8RWCxj1ZxKJVUYZqpQ9LsJq6K';
+//
+// var cp     = require('child_process'),
+// 	assert = require('assert'),
+// 	connector;
+//
+// describe('Connector', function () {
+// 	this.slow(5000);
+//
+// 	after('terminate child process', function (done) {
+// 		this.timeout(7000);
+//
+//         setTimeout(function(){
+//             connector.kill('SIGKILL');
+// 			done();
+//         }, 5000);
+// 	});
+//
+// 	describe('#spawn', function () {
+// 		it('should spawn a child process', function () {
+// 			assert.ok(connector = cp.fork(process.cwd()), 'Child process not spawned.');
+// 		});
+// 	});
+//
+// 	describe('#handShake', function () {
+// 		it('should notify the parent process when ready within 5 seconds', function (done) {
+// 			this.timeout(5000);
+//
+// 			connector.on('message', function (message) {
+// 				if (message.type === 'ready')
+// 					done();
+// 			});
+//
+// 			connector.send({
+// 				type: 'ready',
+// 				data: {
+// 					options: {
+// 						user: USER,
+// 						token: TOKEN
+// 					}
+// 				}
+// 			}, function (error) {
+// 				assert.ifError(error);
+// 			});
+// 		});
+// 	});
+//
+// 	describe('#data', function (done) {
+// 		it('should process the JSON data', function () {
+// 			connector.send({
+// 				type: 'data',
+// 				data: {
+//                     message: 'This is a test message from Pushover Connector Plugin',
+//                     title: 'Test Message',
+//                     sound: 'default',
+//                     device: 'm1note',
+//                     priority: 1,
+//                     url: 'http://reekoh.com',
+//                     url_title: 'Reekoh Official Website'
+// 				}
+// 			}, done);
+// 		});
+// 	});
+//
+// 	describe('#data', function (done) {
+// 		it('should process the Array data', function () {
+// 			connector.send({
+// 				type: 'data',
+// 				data: [
+// 					{
+// 						message: 'This is a test message from Pushover Connector Plugin',
+// 						title: 'Test Message',
+// 						sound: 'default',
+// 						device: 'm1note',
+// 						priority: 1,
+// 						url: 'http://reekoh.com',
+// 						url_title: 'Reekoh Official Website'
+// 					},
+// 					{
+// 						message: 'This is a test message from Pushover Connector Plugin',
+// 						title: 'Test Message',
+// 						sound: 'default',
+// 						device: 'm1note',
+// 						priority: 1,
+// 						url: 'http://reekoh.com',
+// 						url_title: 'Reekoh Official Website'
+// 					}
+// 				]
+// 			}, done);
+// 		});
+// 	});
+// });
